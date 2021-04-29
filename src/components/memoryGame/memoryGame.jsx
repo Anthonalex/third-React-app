78ac89966484701f574735a4e8e3e9a9051c8257
Anthonalex <Anthonalex@users.noreply.github.com>
2021-04-29 21:34:52 +0400

    first task completed,head to reset data when player lose or win and want play again

--- src/components/memoryGame/memoryGame.jsx
+++ src/components/memoryGame/memoryGame.jsx
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./memoryGame.css";
+import { dataBase } from "../../service/data";
 
-export default class MemoryGame extends React.Component {
-  constructor(props) {
-    super(props);
+export default function MeomoryGame() {
+  const [data, setData] = useState(dataBase);
+  const [score, setScore] = useState(0);
 
-    this.state = {
-      name: "foo",
-    };
-  }
+  const handleClick = (e) => {
+    data.map((el) => {
+      if (el.image === e.target.src) {
+        if (el.clicked === false) {
+          setScore(score + 1);
+          el.clicked = true;
+        } else if (el.clicked === true) {
+          setScore(0);
+        }
+      }
+      return;
+    });
 
-  render() {
-    return (
-      <div className="memory-game-container">
-        <h1>Memory Game.</h1>
+    data.sort((a, b) => 0.5 - Math.random());
+    RenderData();
+  };
+
+  const RenderData = () => {
+    return data.map((el) => {
+      return (
+        <div key={el.id}>
+          <img src={el.image} alt={el.name} onClick={handleClick} id={el.id} />
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h1>Memory Game</h1>
+      <h5>
+        You are presented with multiple images of celebrities. The images get
+        shuffled every time they are clicked. User CAN NOT click on any image
+        more than once or else the score resets to zero.
+      </h5>
+
+      <div className="score-state">
+        Score : {score} <b>{score === 8 && "You win,Goodjob!"}</b>
       </div>
-    );
-  }
+
+      <div className="memory-game-container">
+        <RenderData />
+      </div>
+    </div>
+  );
 }
---
+++ src/service/data.js
@@ -0,0 +1,98 @@
+export const dataBase = [
+  {
+    id: 1,
+    clicked: false,
+    name: "Angelina Jolie Voight",
+    image:
+      "http://beauty-around.com/images/sampledata/Hollywood_Actress/1Anjelina-Jolie.jpg",
+    occupation: "Actress, Director",
+  },
+  {
+    id: 2,
+    clicked: false,
+    name: "Charlize Theron",
+    image:
+      "http://beauty-around.com/images/sampledata/Hollywood_Actress/2sharliz.jpg",
+    occupation: "Model, Actress",
+  },
+  {
+    id: 3,
+    clicked: false,
+    name: "Jessica Alba",
+    image:
+      "http://beauty-around.com/images/sampledata/Hollywood_Actress/3Alba.jpg",
+    occupation: "Model, Actress",
+  },
+  {
+    id: 4,
+    clicked: false,
+    name: "Megan Denise Fox",
+    image:
+      "http://beauty-around.com/images/sampledata/Hollywood_Actress/4Megan_Fox.jpg",
+    occupation: "Model, Actress",
+  },
+  {
+    id: 5,
+    clicked: false,
+    name: "Keira Knightley",
+    image:
+      "http://beauty-around.com/images/sampledata/Hollywood_Actress/6Keira_Knightley.jpg",
+    occupation: "Actress",
+  },
+  {
+    id: 6,
+    clicked: false,
+    name: "Nina Dobrev",
+    image:
+      "http://beauty-around.com/images/sampledata/Hollywood_Actress/8nina_Dobrev.jpg",
+    occupation: "Actress",
+  },
+  {
+    id: 7,
+    clicked: false,
+    name: "Maria Bellucci",
+    image:
+      "http://beauty-around.com/images/sampledata/Hollywood_Actress/9Monica.jpg",
+    occupation: "Model, Actress",
+  },
+  {
+    id: 8,
+    clicked: false,
+    name: "Justin Bieber",
+    image:
+      "https://static.billboard.com/files/2020/10/Justin-Bieber-Justin-Bieber-Seasons-Premiere-2020-Billboard-1548-1604003367-compressed.jpg",
+    occupation: "Singer",
+  },
+  {
+    id: 9,
+    clicked: false,
+    name: "Daddy Yankee",
+    image:
+      "https://media1.fdncms.com/orlando/imager/u/original/2514354/gal_sel_daddy-yankee-2016.jpg",
+    occupation: "Singer",
+  },
+  {
+    id: 10,
+    clicked: false,
+    name: "Sean Paul",
+    image:
+      "https://www.thepeninsulaqatar.com/uploads/2018/09/20/post_main_cover_fit//1b21da4888de17e14120771c191d5373a06fe9c1.jpg",
+    occupation: "Singer",
+  },
+  {
+    id: 11,
+    clicked: false,
+    name: "Deadpool",
+    image:
+      "https://vignette.wikia.nocookie.net/marvelmovies/images/6/66/Deadpool_promo.png/revision/latest?cb=20150707202600",
+    occupation: "Hero",
+  },
+  {
+    id: 12,
+    clicked: false,
+    name: "Sofia Vergara",
+    image:
+      "https://i.dailymail.co.uk/i/newpix/2018/09/18/20/5017C57300000578-6182201-image-a-100_1537300563965.jpg",
+    occupation: "Actress",
+  },
+];
