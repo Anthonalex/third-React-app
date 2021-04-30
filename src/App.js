12d8b6ffd866f548a01b410f90d5f88e88e2f11b
Anthonalex <Anthonalex@users.noreply.github.com>
2021-04-30 17:37:07 +0400

    page routes created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import "./App.css";
-import MemoryGame from "./components/memoryGame/memoryGame";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import MemoryGame from "./components/MemoryGame/memoryGame";
+import WeatherTracker from "./components/WeatherTracker/weatherTracker";
 
 function App() {
   return (
-    <div>
-      <MemoryGame />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/MemoryGame">Memory Game</Link>
+            </li>
+            <li>
+              <Link to="/WeatherTracker">Weather Tracker App</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/MemoryGame">
+            <MemoryGame />
+          </Route>
+          <Route path="/WeatherTracker">
+            <WeatherTracker />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+function Home() {
+  return (
+    <div className="home-page-container">
+      <h1>Welcome to my third-React-app</h1>
+      <p>please select your page and enjoy!</p>
+    </div>
+  );
+}
---
+++ src/components/WeatherTracker/weatherTracker.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import "./weatherTracker.css";
+
+export default function WeatherTracker() {
+  
+
+
+
+
+
+  return (
+    <div>
+      <h1>Weather Tracker</h1>
+    </div>
+  );
+}
