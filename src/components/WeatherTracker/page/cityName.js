dde361de91839e6ee21b7fc6a19a9424853caf62
Anthonalex <Anthonalex@users.noreply.github.com>
2021-05-01 16:47:12 +0400

    fetched data render function done

--- src/components/WeatherTracker/page/cityName.js
+++ src/components/WeatherTracker/page/cityName.js
@@ -1,8 +1,24 @@
+/* eslint-disable array-callback-return */
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { citiesData } from "../../../service/data";
 
 export default function CityName() {
   let { cityName } = useParams();
+  const [fetchedData, setFetchedData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d93da1ec68ba5770af2db5ad738e157c`
+    )
+      .then((r) => {
+        return r.json();
+      })
+      .then((weatherData) => {
+        setFetchedData(weatherData.main);
+        
+      });
+  }, [cityName]);
 
   const RenderCities = () => {
     return citiesData.map((el) => {
@@ -17,9 +33,48 @@ export default function CityName() {
     });
   };
 
+
+  const RenderData = () => {
+    return (
+      <div>
+        <p>
+          Temp :{" "}
+          <b>
+            {fetchedData.temp}K ({parseInt(fetchedData.temp - 273.15)}°C)
+          </b>
+        </p>
+        <p>
+          Feels Like :{" "}
+          <b>
+            {fetchedData.feels_like}K (
+            {parseInt(fetchedData.feels_like - 273.15)}°C)
+          </b>
+        </p>
+        <p>
+          Pressure : <b>{fetchedData.pressure}mb</b>
+        </p>
+        <p>
+          Max Temp :{" "}
+          <b>
+            {fetchedData.temp_max}K ({parseInt(fetchedData.temp_max - 273.15)}
+            °C)
+          </b>
+        </p>
+        <p>
+          Min Temp :{" "}
+          <b>
+            {fetchedData.temp_min}K ({parseInt(fetchedData.temp_min - 273.15)}
+            °C)
+          </b>
+        </p>
+      </div>
+    );
+  };
+
   return (
     <div>
       <RenderCities />
+      <RenderData />
     </div>
   );
 }
